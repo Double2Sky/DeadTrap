d0f5a26c28ed66c44dfec13b270ab21604f55856
Mike Salvatore <mike.salvatore@canonical.com>
2020-08-16 14:32:45 -0400

    Add installation step for "requests" library to setup.py
    DeadTrap relies on the requests python library, but the install script
    does not install it.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ os.system("sudo apt install python3-pip")
 os.system("pip3 install -U selenium")
 os.system("pip3 install -U bs4")
 os.system("pip3 install -U configobj")
+os.system("pip3 install -U requests")
 
 print("\n \n {} \n \n".format(OS_bit))
 
